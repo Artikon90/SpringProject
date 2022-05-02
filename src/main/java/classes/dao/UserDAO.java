ce37e34faf9e59e0857e7fc95659083be5c42ec9
Artikon90 <artikon90@gmail.com>
2022-05-02 04:21:46 +0300

    Переход на JDBC: сделал чтение записей

--- src/main/java/classes/dao/UserDAO.java
+++ src/main/java/classes/dao/UserDAO.java
@@ -3,36 +3,59 @@ package classes.dao;
 import classes.models.User;
 import org.springframework.stereotype.Component;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class UserDAO {
-    private List<User> allUsers;
     private static int userCounter;
+    private static final String URL = "jdbc:postgresql://localhost:5432/spring_project";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "123456";
 
-    {
-        allUsers = new ArrayList<>();
-        allUsers.add(new User(++userCounter, "Jack", "aboba@mail.com", 24));
-        allUsers.add(new User(++userCounter, "Sasha", "google@google.com", 43));
-        allUsers.add(new User(++userCounter, "Glenn", "glenn123@noob.org", 28));
-        allUsers.add(new User(++userCounter, "Mark", "marksman@yahoo.com", 32));
-        allUsers.add(new User(++userCounter, "Mary", "todotodo@test.jp", 10));
-        allUsers.add(new User(++userCounter, "Alex", "oldman@rambler.org", 64));
-        allUsers.add(new User(++userCounter, "Max", "maximus@google.nl", 31));
+    private static Connection connection;
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
+
     public List<User> getAllUsers() {
-        return allUsers;
+        List<User> allUserList = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet allUsersSet = statement.executeQuery("SELECT * FROM alluser");
+            while(allUsersSet.next()) {
+                User user = new User();
+                user.setId(allUsersSet.getInt("id"));
+                user.setName(allUsersSet.getString("name"));
+                user.setEmail(allUsersSet.getString("email"));
+                user.setAge(allUsersSet.getInt("age"));
+                allUserList.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return allUserList;
     }
 
     public User getUserById(int id) {
-        return allUsers.stream().filter(user -> user.getId() == id).findAny().orElse(null);
+        return null;
+//        return allUsers.stream().filter(user -> user.getId() == id).findAny().orElse(null);
     }
 
     public void addUser(User user) {
-        user.setId(++userCounter);
-        allUsers.add(user);
+//        user.setId(++userCounter);
+//        allUsers.add(user);
     }
 
     public void editUser(int id, User user) {
@@ -43,6 +66,6 @@ public class UserDAO {
     }
 
     public void deleteUser(int id) {
-        allUsers.removeIf(p -> p.getId() == id);
+//        allUsers.removeIf(p -> p.getId() == id);
     }
 }
