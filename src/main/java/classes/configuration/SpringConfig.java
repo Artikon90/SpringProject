c840196cb295462e68bbd7640b7a5f2f6f9925af
Artikon90 <artikon90@gmail.com>
2022-05-03 22:40:28 +0300

    Добавил JDBC Template,

--- src/main/java/classes/configuration/SpringConfig.java
+++ src/main/java/classes/configuration/SpringConfig.java
@@ -1,10 +1,14 @@
 package classes.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,13 +16,23 @@ import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
+import javax.sql.DataSource;
+import java.util.Properties;
+
 @Configuration
 @ComponentScan("classes")
 @EnableWebMvc
+@PropertySource("classpath:application.properties")
 public class SpringConfig implements WebMvcConfigurer {
-
+    @Value("${spring.datasource.driver-class-name}")
+        String classname;
+    @Value("${spring.datasource.url}")
+        String url;
+    @Value("${spring.datasource.username}")
+        String username;
+    @Value("${spring.datasource.password}")
+        String password;
     private final ApplicationContext applicationContext;
-
     @Autowired
     public SpringConfig(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
@@ -46,5 +60,18 @@ public class SpringConfig implements WebMvcConfigurer {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
+    }
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(classname);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
     }
 }
--- src/main/java/classes/dao/UserDAO.java
+++ src/main/java/classes/dao/UserDAO.java
@@ -1,108 +1,43 @@
 package classes.dao;
 
 import classes.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.Collections;
+//import java.sql.*;
+//import java.util.ArrayList;
+//import java.util.Collections;
 import java.util.List;
 
 @Component
 public class UserDAO {
-    private static final String URL = "jdbc:postgresql://localhost:5432/spring_project";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "123456";
-
-    private static Connection connection;
-    static {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+    private final JdbcTemplate jdbcTemplate;
+@Autowired
+    public UserDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
-
     public List<User> getAllUsers() {
-        List<User> allUserList = new ArrayList<>();
-        try {
-            Statement statement = connection.createStatement();
-            ResultSet allUsersSet = statement.executeQuery("SELECT * FROM alluser");
-            while(allUsersSet.next()) {
-                User user = new User();
-                user.setId(allUsersSet.getInt("id"));
-                user.setName(allUsersSet.getString("name"));
-                user.setEmail(allUsersSet.getString("email"));
-                user.setAge(allUsersSet.getInt("age"));
-                allUserList.add(user);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        Collections.sort(allUserList);
-        return allUserList;
+        return jdbcTemplate.query("SELECT * FROM alluser", new BeanPropertyRowMapper<>(User.class));
     }
 
     public User getUserById(int id) {
-        User user = null;
-        try {
-            PreparedStatement getUserSql = connection.prepareStatement(
-                    "SELECT * FROM alluser WHERE id = ?");
-            getUserSql.setInt(1, id);
-            ResultSet res = getUserSql.executeQuery();
-            user = new User();
-            res.next();
-            user.setId(res.getInt("id"));
-            user.setName(res.getString("name"));
-            user.setEmail(res.getString("email"));
-            user.setAge(res.getInt("age"));
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return user;
+        return jdbcTemplate.query("SELECT * FROM alluser WHERE id=?",
+                new BeanPropertyRowMapper<>(User.class), id).stream().findAny().orElse(null);
     }
 
     public void addUser(User user) {
-        try {
-            PreparedStatement addUserSql = connection.prepareStatement(
-                    "INSERT INTO alluser(name, email, age) VALUES (?, ?, ?)");
-            addUserSql.setString(1, user.getName());
-            addUserSql.setString(2, user.getEmail());
-            addUserSql.setInt(3, user.getAge());
-            addUserSql.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        jdbcTemplate.update("INSERT INTO alluser(name, email, age) VALUES(?, ?, ?)",
+                            user.getName(), user.getEmail(), user.getAge());
     }
 
     public void editUser(int id, User userUpdate) {
-        try {
-            PreparedStatement editUserSql = connection.prepareStatement(
-                    "UPDATE alluser SET name=?, email=?, age=? WHERE id=?");
-            editUserSql.setString(1, userUpdate.getName());
-            editUserSql.setString(2, userUpdate.getEmail());
-            editUserSql.setInt(3, userUpdate.getAge());
-            editUserSql.setInt(4, id);
-            editUserSql.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        jdbcTemplate.update("UPDATE alluser SET name = ?, email = ?, age = ? WHERE id = ?",
+                userUpdate.getName(), userUpdate.getEmail(), userUpdate.getAge(), id);
     }
 
     public void deleteUser(int id) {
-        try {
-            PreparedStatement deleteUserSql = connection.prepareStatement(
-                "DELETE FROM alluser WHERE id=?");
-            deleteUserSql.setInt(1, id);
-            deleteUserSql.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
+        jdbcTemplate.update("DELETE FROM alluser WHERE id = ?", id);
     }
 }
